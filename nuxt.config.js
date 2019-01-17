ca99566f24db80079a94451a731c0b2c52b0997c
KoushiKagawa <koushi.kagawa@resinc.co.jp>
2019-01-17 17:11:02 +0900

    edit config

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,11 @@
+//const config = require('./.contentful.json')
+
+const contentful = require('contentful')
 const config = require('./.contentful.json')
+const client = contentful.createClient({
+  space: config.CTF_SPACE_ID,
+  accessToken: config.CTF_CDA_ACCESS_TOKEN
+})
 
 module.exports = {
   env: {
